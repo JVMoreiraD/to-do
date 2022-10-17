b2620c6be552a9b2eb7a72307bf756877423aa8e
jvmoreirad <jvitormoreirad@hotmail.com>
2022-10-17 14:55:45 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './styles/global.css'
-import { Header } from './components/Header/Header'
-import { TextInput } from './components/TextInput/Input'
-import { PageContent } from './components/PageContent/PageContent'
-import { Button } from './components/Button/Button'
-import { PlusCircle } from 'phosphor-react'
-import { SignIn } from './pages/SignIn/SignIn'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './routes'
 
 function App() {
 
   return (
-    <SignIn></SignIn>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
 
